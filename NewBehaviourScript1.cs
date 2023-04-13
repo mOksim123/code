feeb1712787e0fe1c742cc043631149c745cb100
mOksim123 <129955576+mOksim123@users.noreply.github.com>
2023-04-13 21:11:14 +0500

    Add files via upload

---
+++ NewBehaviourScript1.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    //здоровье NPC
+    int health = 5;
+
+    //уровень NPC
+    int level = 1;
+
+    //скорость NPC
+    float speed = 1.2f;
+
+    void Start()
+    {
+    
+    }
+
+    void Update()
+    {
+
+    }
+}
+
